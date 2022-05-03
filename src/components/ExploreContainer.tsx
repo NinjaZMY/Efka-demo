4f71239fff24d15bc5869a141b46f1e6ba099095
NinjaZMY <modtafrik@gmail.com>
2022-05-03 23:14:24 +0100

    first commit- design not responsive

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,24 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+ const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+  <div className="container home">
+    <img id='logo' src="assets/logo.gif">
+    </img>
+  
+    <div className='bgSize'>
+    <div id="StarterMessage" className="text" ></div>
+    </div>
+   {/*     <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+       */ }
+  </div> 
+  
+
+  );
+  
+};
+
+export default ExploreContainer;
+ 
